ded8dd809675a456deff92e4c4c284d516d0524b
evd1ser <evd1ser@gmail.com>
2016-09-15 18:54:31 +0300

    добавил боковое меню

--- app/src/main/java/ru/evd1ser/moneytracker/MainActivity.java
+++ app/src/main/java/ru/evd1ser/moneytracker/MainActivity.java
@@ -1,16 +1,64 @@
 package ru.evd1ser.moneytracker;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * homework 2
+     */
+    private Toolbar toolbar;
+    private void setupActionBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar); setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+
+    private void setupDrawerLayout() {
+        toggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        setTitle(getString(R.string.app_name));
+    }
+
+    @Override public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override public boolean onNavigationItemSelected(MenuItem item) {
+        if (drawerLayout != null) { drawerLayout.closeDrawer(GravityCompat.START); } return true;
+    }
+
+    /**
+     * homework 1
+     */
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         Log.d(LOG_TAG, "onCreate");
     }
 
