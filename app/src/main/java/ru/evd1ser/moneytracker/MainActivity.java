47b1e8c3d140141b456d558c002927ea585d27d1
evd1ser <evd1ser@gmail.com>
2016-09-15 20:37:59 +0300

    Поправил манифест для отображения без основного бара

--- app/src/main/java/ru/evd1ser/moneytracker/MainActivity.java
+++ app/src/main/java/ru/evd1ser/moneytracker/MainActivity.java
@@ -82,28 +82,28 @@ public class MainActivity extends AppCompatActivity
      */
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.d(LOG_TAG, "onStart");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.d(LOG_TAG, "onResume");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.d(LOG_TAG, "onStop");
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Log.d(LOG_TAG, "onRestart");
+//    }
 
 }
